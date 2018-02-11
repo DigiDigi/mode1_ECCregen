57a64ac7191f63fd8d9905c9f313c03da03a0c5a
DigiDigi <DigiDigi@gmail.com>
2018-02-10 22:06:32 -0600

    WIP

---
+++ test_patched.py
@@ -0,0 +1,12 @@
+
+# Testing if a patching with an unmodified binary results in the same file.
+
+# source.iso -> [splitter.py] -> split.bin, sync.txt
+# source.iso, split.bin, sync.txt -> [patcher.py] -> patched.iso
+# (source.iso == patched.iso) -> test_patched.py
+
+
+
+
+
+
