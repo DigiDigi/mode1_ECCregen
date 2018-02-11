76fb98cb7e16c64752fe7cf7c56ea4daf819d51b
DigiDigi <DigiDigi@gmail.com>
2018-02-10 21:55:47 -0600

    WIP

---
+++ test_rejoined.py
@@ -0,0 +1,12 @@
+
+# Testing if rejoining split headers and regenerating the edc/ecc result in the same file.
+
+# source.iso -> [splitter.py] -> split.bin, sync.txt
+# source.iso, split.bin, sync.txt -> [rejoin.py] -> rejoined.iso
+# (source.iso == rejoined.iso) -> test_rejoined.py
+
+
+
+
+
+
