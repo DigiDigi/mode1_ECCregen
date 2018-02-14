7840a6bf703550a4039a9f579d11ace66ff747f7
DigiDigi <DigiDigi@gmail.com>
2018-02-13 19:01:29 -0600

    WIP

--- apply_patch.py
+++ apply_patch.py
@@ -34,36 +34,52 @@ patchfile = open(filename2, 'r')
 syncfile = open(filename3, 'r')
 patchedfile = open(filename4, 'r+b')
 
+syncs = syncfile.readlines()
+
 global pf_line
-global last_block
 global current_index
 global apply_bytes
+global last_block
 pf_line = ['']
-last_block = -1
 current_index = 0  # ByteIndex after 16byte sync/header.
 apply_bytes = ''
 
 pf_line[0] = patchfile.readline()
+s_line = pf_line[0].split(' ')
+pf_index = int(s_line[0])
+current_block = pf_index / 2048
+pf_index -= (2048 * current_block)
+last_block = current_block
+
 while pf_line[0]:
     s_line = pf_line[0].split(' ')
     s_len = len(s_line[1])
-    byte_list = lst = [s_line[1][i:i+2] for i in xrange(0, s_len - s_len % 2, 2)]
+    pf_bytes = lst = [s_line[1][i:i+2] for i in xrange(0, s_len - s_len % 2, 2)]
     pf_index = int(s_line[0])
-    current_block = (pf_index + 1) / 2048
+    current_block = pf_index / 2048
+    pf_index -= (2048 * current_block)
+    current_index = int(syncs[current_block]) + 16
+
     if current_block > last_block:
-        current_index = int(syncfile.readline()) + 16
-        last_block = current_block
-        # EDC/ECC Regen.
         patchedfile.seek(current_index - 16)
         block_data = patchedfile.read(2048+16)
+        patchedfile.write(cdrom_ecc.get_edc_ecc(block_data)[0])
+        patchedfile.write(chr(0) * 8)
         patchedfile.write(cdrom_ecc.get_edc_ecc(block_data)[1])
+        last_block = current_block
 
     patchedfile.seek(current_index + pf_index)
     apply_bytes = ''
-    for b in byte_list:
+    for b in pf_bytes:
         apply_bytes = apply_bytes + chr(int(b, 16))
     patchedfile.write(apply_bytes)
     pf_line[0] = patchfile.readline()
+
+patchedfile.seek(current_index - 16)
+block_data = patchedfile.read(2048 + 16)
+patchedfile.write(cdrom_ecc.get_edc_ecc(block_data)[0])
+patchedfile.write(chr(0) * 8)
+patchedfile.write(cdrom_ecc.get_edc_ecc(block_data)[1])
 
 print 'Done.'
 
